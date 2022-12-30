d26104977a90fc11bf513cbe483aec5be0a46c0e
florianstrobl <FlorianStrobl@outlook.de>
2022-12-30 13:32:24 +0100

    .

--- Backend/main.py
+++ Backend/main.py
@@ -23,15 +23,8 @@ tmp.setData(
 )
 
 # tmp.setData(tmp.editData("""int((absolute_position1).split(", ")[0][1:]) - int((absolute_position2).split(", ")[0][1:]) && block_type1 - block_type2""", "sort"))
-tmp.setData(tmp.editData("""
-session_begin <- random.randint(5, 12) &&
-
-session_begin1 - session_begin2 &&
-
-(0 if role1 == role2 else (-1 if role1 == "Player" else (1 if role2 == "Player" else (-1 if role1 == "Moderator" else 1)))) &&
-
-session_begin <= 8
-"""))
+#tmp.setData(tmp.editData("""(session_begin) <- random.randint(5, 12) &&session_begin1 - session_begin2 &&(0 if role1 == role2 else (-1 if role1 == "Player" else (1 if role2 == "Player" else (-1 if role1 == "Moderator" else 1)))) &&session_begin <= 8"""))
+tmp.setData(tmp.editData("""(session_begin) <- random.randint(5, 12)"""))
 
 updateDataInDB(cursor, tmp)
 
